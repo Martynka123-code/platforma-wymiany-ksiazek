22d8377cb90e2fdb81e0c207163851c19d759046
damia <damian.kantor677@gmail.com>
2026-06-15 21:22:20 +0200

    damian nowa procedura

--- skrypt_glowny.sql
+++ skrypt_glowny.sql
@@ -185,6 +185,326 @@ CREATE TRIGGER trg_po_dodaniu_transakcji
 AFTER INSERT ON Transakcje FOR EACH ROW EXECUTE FUNCTION zaktualizuj_status_ofert();
 
 
+
+
+-- Czyszczenie starego widoku, jeśli istniał
+DROP VIEW IF EXISTS Widok_Pelne_Transakcje CASCADE;
+
+-- Tworzenie widoku ogólnego transakcji
+CREATE VIEW Widok_Pelne_Transakcje AS
+SELECT
+    t.ID_Transakcji,
+    t.Data_Wymiany,
+    t.Status_Transakcji,
+
+    -- STRONA INICJUJĄCA (Co i kto oddaje)
+    u_od.Login AS Uzytkownik_Inicjujacy,
+    k_od.Tytul AS Ksiazka_Oddawana,
+
+    -- STRONA ODPOWIADAJĄCA (Co i kto daje w zamian)
+    u_za.Login AS Uzytkownik_Akceptujacy,
+    k_za.Tytul AS Ksiazka_Otrzymywana
+
+FROM Transakcje t
+-- 1. Dołączenie danych dla oferty OD (ktoś daje tę książkę)
+JOIN Oferty_Uzytkownikow o_od ON t.ID_Oferty_Od = o_od.ID_Oferty
+JOIN Uzytkownicy u_od ON o_od.ID_Uzytkownika = u_od.ID_Uzytkownika
+JOIN Ksiazki k_od ON o_od.ID_Ksiazki = k_od.ID_Ksiazki
+
+-- 2. Dołączenie danych dla oferty ZA (ktoś daje tę książkę w zamian)
+JOIN Oferty_Uzytkownikow o_za ON t.ID_Oferty_Za = o_za.ID_Oferty
+JOIN Uzytkownicy u_za ON o_za.ID_Uzytkownika = u_za.ID_Uzytkownika
+JOIN Ksiazki k_za ON o_za.ID_Ksiazki = k_za.ID_Ksiazki;
+
+
+
+-- Czyszczenie starego widoku, jeśli istniał
+DROP VIEW IF EXISTS Widok_Pelne_Oferty CASCADE;
+
+-- Tworzenie widoku ogólnego
+CREATE VIEW Widok_Pelne_Oferty AS
+SELECT
+    o.ID_Oferty,
+    u.Login AS Uzytkownik_Proponujacy,
+    k.Tytul AS Tytul_Ksiazki,
+    o.Opis_Stanu AS Stan_Egzemplarza,
+    s.Nazwa AS Status_Oferty
+FROM Oferty_Uzytkownikow o
+-- Dołączenie tabeli Użytkownicy (Moduł Osoby 1)
+JOIN Uzytkownicy u ON o.ID_Uzytkownika = u.ID_Uzytkownika
+-- Dołączenie tabeli Książki (Moduł Osoby 2)
+JOIN Ksiazki k ON o.ID_Ksiazki = k.ID_Ksiazki
+-- Dołączenie tabeli Statusy_Ofert (Twój Moduł)
+JOIN Statusy_Ofert s ON o.ID_Statusu = s.ID_Statusu;
+
+
+
+
+
+CREATE OR REPLACE PROCEDURE proc_Dodaj_Autora_i_Ksiazke(
+    p_imie VARCHAR(100),
+    p_nazwisko VARCHAR(100),
+    p_tytul VARCHAR(300),
+    p_isbn VARCHAR(17),
+    p_rok SMALLINT,
+    p_id_wydawnictwa INT,
+    p_id_kategorii INT
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    v_id_autora INT;
+BEGIN
+
+    INSERT INTO Autorzy(Imie, Nazwisko)
+    VALUES(p_imie, p_nazwisko)
+    RETURNING ID_Autora INTO v_id_autora;
+
+    INSERT INTO Ksiazki(
+        Tytul,
+        ISBN,
+        Rok_Wydania,
+        ID_Autora,
+        ID_Wydawnictwa,
+        ID_Kategorii
+    )
+    VALUES(
+        p_tytul,
+        p_isbn,
+        p_rok,
+        v_id_autora,
+        p_id_wydawnictwa,
+        p_id_kategorii
+    );
+
+EXCEPTION
+    WHEN OTHERS THEN
+        RAISE NOTICE 'Wystapil blad: %', SQLERRM;
+        RAISE;
+END;
+$$;
+
+
+-- Użycie
+CALL proc_Dodaj_Autora_i_Ksiazke(
+    'Terry',
+    'Pratchett',
+    'Kolor magii',
+    '978-83-123-4567-8',
+    1983::smallint,
+    1,
+    1
+);
+
+
+CREATE OR REPLACE PROCEDURE proc_Dodaj_Recenzje(
+    p_ocena INT,
+    p_tresc TEXT,
+    p_id_ksiazki INT,
+    p_id_uzytkownika INT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    INSERT INTO Recenzje_Ksiazek(
+        Wartosc,
+        Tresc,
+        ID_Ksiazki,
+        ID_Uzytkownika
+    )
+    VALUES(
+        p_ocena,
+        p_tresc,
+        p_id_ksiazki,
+        p_id_uzytkownika
+    );
+END;
+$$;
+
+
+-- Użycie
+CALL proc_Dodaj_Recenzje(
+    10,
+    'Świetna książka',
+    1,
+    1
+);
+
+
+CREATE OR REPLACE PROCEDURE proc_Dodaj_Oferte(
+    p_login VARCHAR(50),
+    p_tytul_ksiazki VARCHAR(300),
+    p_opis_stanu VARCHAR(255)
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    v_id_uzytkownika INT;
+    v_id_ksiazki INT;
+    v_id_statusu INT;
+BEGIN
+    -- 1. Znalezienie ID użytkownika na podstawie jego loginu (Moduł Osoby 1)
+    SELECT ID_Uzytkownika INTO v_id_uzytkownika 
+    FROM Uzytkownicy 
+    WHERE Login = p_login;
+
+    IF v_id_uzytkownika IS NULL THEN
+        RAISE EXCEPTION 'Błąd walidacji: Użytkownik o loginie "%" nie istnieje!', p_login;
+    END IF;
+
+    -- 2. Znalezienie ID książki na podstawie jej tytułu (Moduł Osoby 2)
+    -- Używamy ILIKE, aby wielkość liter w tytule nie miała znaczenia
+    SELECT ID_Ksiazki INTO v_id_ksiazki 
+    FROM Ksiazki 
+    WHERE Tytul ILIKE p_tytul_ksiazki;
+
+    IF v_id_ksiazki IS NULL THEN
+        RAISE EXCEPTION 'Błąd walidacji: Książka o tytule "%" nie istnieje w katalogu!', p_tytul_ksiazki;
+    END IF;
+
+    -- 3. Automatyczne pobranie ID statusu 'Dostępna' dla nowej oferty (Twój słownik)
+    SELECT ID_Statusu INTO v_id_statusu 
+    FROM Statusy_Ofert 
+    WHERE Nazwa = 'Dostępna';
+
+    IF v_id_statusu IS NULL THEN
+        RAISE EXCEPTION 'Błąd systemowy: Status "Dostępna" nie istnieje w słowniku statusów!';
+    END IF;
+
+    -- 4. Wstawienie nowej oferty do bazy danych
+    INSERT INTO Oferty_Uzytkownikow (Opis_Stanu, ID_Uzytkownika, ID_Ksiazki, ID_Statusu)
+    VALUES (p_opis_stanu, v_id_uzytkownika, v_id_ksiazki, v_id_statusu);
+
+    RAISE NOTICE 'Sukces: Oferta dla książki "%" została dodana przez użytkownika %.', p_tytul_ksiazki, p_login;
+
+EXCEPTION
+    WHEN OTHERS THEN
+        RAISE NOTICE 'Wystąpił nieoczekiwany błąd podczas dodawania oferty: %', SQLERRM;
+        RAISE;
+END;
+$$;
+
+CALL proc_Dodaj_Oferte(
+    'admin', 
+    'Kolor magii', 
+    'Stan bardzo dobry, drobne rysy na okładce'
+);
+
+
+
+
+CREATE OR REPLACE PROCEDURE proc_Zmien_Status_Oferty(
+    p_id_oferty INT,
+    p_id_statusu INT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    UPDATE Oferty_Uzytkownikow
+    SET ID_Statusu = p_id_statusu
+    WHERE ID_Oferty = p_id_oferty;
+END;
+$$;
+
+-- Użycie
+CALL proc_Zmien_Status_Oferty(1, 2);
+
+
+
+CREATE OR REPLACE PROCEDURE proc_Dodaj_Uzytkownika(
+    p_login VARCHAR(50),
+    p_haslo VARCHAR(255),
+    p_email VARCHAR(100),
+    p_id_roli INT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    INSERT INTO Uzytkownicy(
+        Login,
+        Haslo,
+        Email,
+        ID_Roli
+    )
+    VALUES(
+        p_login,
+        p_haslo,
+        p_email,
+        p_id_roli
+    );
+END;
+$$;
+
+-- Użycie
+CALL proc_Dodaj_Uzytkownika(
+    'nowy_user',
+    'tajnehaslo',
+    'nowy@wp.pl',
+    2
+);
+
+
+
+CREATE OR REPLACE PROCEDURE proc_Usun_Recenzje(
+    p_id_recenzji INT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    DELETE FROM Recenzje_Ksiazek
+    WHERE ID_Recenzji = p_id_recenzji;
+END;
+$$;
+
+
+-- Użycie
+CALL proc_Usun_Recenzje(1);
+
+
+
+CREATE OR REPLACE PROCEDURE proc_Utworz_Transakcje(
+    p_id_oferty_od INT,
+    p_id_oferty_za INT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    INSERT INTO Transakcje(
+        Status_Transakcji,
+        ID_Oferty_Od,
+        ID_Oferty_Za
+    )
+    VALUES(
+        'W toku',
+        p_id_oferty_od,
+        p_id_oferty_za
+    );
+END;
+$$;
+
+
+-- Użycie
+CALL proc_Utworz_Transakcje(1, 2);
+
+
+
+CREATE OR REPLACE PROCEDURE proc_Zmien_Role_Uzytkownika(
+    p_id_uzytkownika INT,
+    p_id_nowej_roli INT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    UPDATE Uzytkownicy
+    SET ID_Roli = p_id_nowej_roli
+    WHERE ID_Uzytkownika = p_id_uzytkownika;
+END;
+$$;
+
+
+-- Użycie
+CALL proc_Zmien_Role_Uzytkownika(1, 1);
+
 -- ====================================================================
 -- FAZA 3: BEZPIECZEŃSTWO I TRANSAKCJE ACID
 -- ====================================================================
